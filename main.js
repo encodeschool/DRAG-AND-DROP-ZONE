259240f0d06d041f989bce159f76d9bf558abd1c
[EN]CODE SCHOOL <146856224+encodeschool@users.noreply.github.com>
2025-02-14 16:44:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let leftBox = document.querySelector(".left");
+let rightBox = document.querySelector(".right");
+let cards = document.querySelectorAll(".card");
+
+for (let card of cards) {
+  card.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    rightBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    rightBox.addEventListener("drop", function (e) {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+
+    leftBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    leftBox.addEventListener("drop", function (e) {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+}
